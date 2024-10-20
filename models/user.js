497375cb378cbabcdb6e3badb838c737d9a571ce
Rafat Alam Saifi <rafatalamsaifi@gmail.com>
2024-10-20 15:30:28 +0530

    fixed usename issue

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: false
     },
     password: {
       type: DataTypes.STRING,
